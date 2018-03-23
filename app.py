f9bb577991d2d1b90fbcc8256c2c043e3ddffe7d
David Mwangi <david.mathenge98@gmail.com>
2018-03-23 17:26:56 +0300

    Add all books API endpoint

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from app import app
+from flask import Flask
+from models import *
+
+app = Flask(__name__)
+api = Api(app)
 
 
 @app.route('/')
-
 def index():
 
-	return ('Hello, World')
+    return 'Hello, World'
+
+
+api.add_resource(Allbooks, '/api/v1/books')
+api.add_resource(Book, '/api/v1/book/<string:book_id>')
+
 
 if __name__ == '__main__':
 
-	app.run()
+    app.run(debug=True)
